2e324ad32f0ebca565fc14c2a860a2e3d27d0d19
FabianaSoft <teixeira.fabiana1@gmail.com>
2024-04-18 14:54:34 -0300

    inserir margens no css inserir espaçamentos no css configurar botoes no css

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-function soma(){
+function soma() {
     let n1 = document.getElementById('n1').value;
     let n2 = document.getElementById('n2').value;
     let resultado = parseInt(n1) + parseInt(n2);
     document.getElementById('resultado').innerHTML = resultado;
 }
 
-function buscar(){
+async function buscar() {
     let pokemon = document.getElementById('pokemon').value;
-    fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('nome').innerHTML = "Nome: " + data.name;
-        document.getElementById('imagem').src = data.sprites.front_default;
-    })
-
-
-
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+    let data = await response.json()
+    document.getElementById('nome').innerHTML = "Nome: " + data.name;
+    document.getElementById('imagem').src = data.sprites.front_default;
 }
 
-function enviar(event){
+async function enviar(event) {
     event.preventDefault();
     let nome = document.getElementById('nome').value;
     let sobrenome = document.getElementById('sobrenome').value;
     let email = document.getElementById('email').value;
     let mensagem = document.getElementById('mensagem').value;
-    let data = {
+    let info = {
         name: nome,
         email: email,
         lastname: sobrenome,
         message: mensagem
     }
-    fetch('https://api-aula.up.railway.app/generica', {
+    let response = await fetch('https://api-aula.up.railway.app/generica', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(info)
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
+    let data = await response.json()
+    console.log(data);
 }
